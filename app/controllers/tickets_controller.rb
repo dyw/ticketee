5fe77ee777892535217641517c48dd0201ce0b2a
dyw <dyw8086@163.com>
2011-10-15 11:13:07 +0800

    Implemented creating tickets for project

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,30 @@
+class TicketsController < ApplicationController
+	
+	def new
+	    @ticket = Ticket.new
+	end
+	def create
+	    @ticket = @project.tickets.build(params[:ticket])
+	    if @ticket.save
+	    	 flash[:notice] = "Ticket has been created."
+	    	 redirect_to [@project, @ticket]
+	    else
+	    	flash[:notice] = "Ticket has not been created."
+	    	render :action => "new"
+	    end
+	end
+	
+	def show
+	end
+	
+	before_filter :find_project
+	before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
+	
+	private
+	def find_project
+	    @project = Project.find(params[:project_id])
+	end
+	def find_ticket
+	    @ticket = @project.tickets.find(params[:id])
+	end
+end
