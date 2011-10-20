8d19818fd7a47c9463e6d2c0cc121c31e0fccee1
dyw <dyw8086@163.com>
2011-10-20 21:27:21 +0800

    first connetion

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,13 +16,13 @@ class ProjectsController < ApplicationController
 	    end
 	end
 	def show
-	    @project = Project.find(params[:id])
+	    find_project #@project = Project.find(params[:id])
 	end
 	def edit
-	    @project = Project.find(params[:id])
+	    find_project #@project = Project.find(params[:id])
 	end
 	def update
-	    @project = Project.find(params[:id])
+	    find_project #@project = Project.find(params[:id])
 	    if @project.update_attributes(params[:project])
 	        flash[:notice] = "Project has been updated."
 	        redirect_to @project
@@ -32,11 +32,21 @@ class ProjectsController < ApplicationController
 	    end
 	end
 	def destroy
-	    @project = Project.find(params[:id])
+	    find_project # @project = Project.find(params[:id])
 	    @project.destroy
 	    flash[:notice] = "The Project has been deleted."
 	    redirect_to projects_path
 	end
 	
 	# before_filter :authorize_admin!, :except => [:index, :show]
+	before_filter :authenticate_user!, :only => [:show]
+	before_filter :find_project, :only => [:show, :edit, :update, :destroy]
+	
+	
+	def find_project
+	     @project = Project.readable_by(current_user).find(params[:id])
+	    
+	end
+	
+	
 end
